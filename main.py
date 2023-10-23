1068986b7897745975375618f95e33285d8bafa7
drmrks87 <dermarx1987@gmail.com>
2023-10-23 20:39:22 +0200

    implemented search

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
     QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, \
     QVBoxLayout, QComboBox
 from PyQt6.QtGui import QAction
 import sys
 import sqlite3
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -22,8 +24,8 @@ class MainWindow(QMainWindow):
         help_menu_item.addAction(about_action)
 
         search_action = QAction("Search", self)
-        search_action.triggered.connect(self.search)
         edit_menu_item.addAction(search_action)
+        search_action.triggered.connect(self.search)
 
         self.table = QTableWidget()
         self.table.setColumnCount(4)
@@ -46,7 +48,7 @@ class MainWindow(QMainWindow):
         dialog.exec()
 
     def search(self):
-        dialog = SearchStudentDialog()
+        dialog = SearchDialog()
         dialog.exec()
 
 
@@ -95,7 +97,8 @@ class InsertDialog(QDialog):
         connection.close()
         main_window.load_data()
 
-class SearchStudentDialog(QDialog):
+
+class SearchDialog(QDialog):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Search Student")
@@ -109,10 +112,25 @@ class SearchStudentDialog(QDialog):
         layout.addWidget(self.student_name)
 
         button = QPushButton("Search")
+        button.clicked.connect(self.search)
         layout.addWidget(button)
 
         self.setLayout(layout)
 
+    def search(self):
+        name = self.student_name.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT * FROM students WHERE name = ?", (name,))
+        rows = list(result)
+        print(rows)
+        items = main_window.table.findItems(name, Qt.MatchFlag.MatchFixedString)
+        for item in items:
+            print(item)
+            main_window.table.item(item.row(), 1).setSelected(True)
+
+        cursor.close()
+        connection.close()
 
 
 app = QApplication(sys.argv)
